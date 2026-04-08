f677ffb3e8e564a94f37be70d00a9e676b61a63e
Agent-smith018 <prajapatismit212@gmail.com>
2026-04-08 17:59:20 -0400

    Refactor server start logic and use environment port

--- src/server.js
+++ src/server.js
@@ -2,16 +2,18 @@ const app = require("./app");
 const { connectDB } = require("./config/db");
 
 const startServer = async () => {
-    try{
-        await connectDB();
+  try {
+    await connectDB();
 
-        app.listen(5000, ()=>{
-            console.log("Server is running...");
-        });
-    } catch(error){
-        console.log("Failed to start server: ", error);
-        process.exit(1);
-    }
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}...`);
+    });
+  } catch (error) {
+    console.log("Failed to start server: ", error);
+    process.exit(1);
+  }
 };
 
 startServer();
